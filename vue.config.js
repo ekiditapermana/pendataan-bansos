3ab492792eeb26b150cff992e4ade988d5ca5dbc
Eki Dita Permana <ekiditapermana@gmail.com>
2023-10-13 14:16:56 +0700

    Initial Commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/pendataan-bansos/" : "/",
+    process.env.NODE_ENV === "production" ? "https://ekiditapermana.github.io/pendataan-bansos/" : "/",
 };
